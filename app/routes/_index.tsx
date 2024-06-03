4bf19bcf4d79c618185915f27ec76708c3ebec51
enZane <ezequiel@octahedroid.com>
2024-06-03 12:24:42 -0600

    :tada: Init Repo

---
+++ app/routes/_index.tsx
@@ -0,0 +1,41 @@
+import type { LoaderFunctionArgs } from "@remix-run/node";
+import type { MetaFunction } from "@remix-run/cloudflare";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    {
+      name: "description",
+      content: "Welcome to Remix! Using Vite and Cloudflare!",
+    },
+  ];
+};
+
+export const loader = async ({ context }: LoaderFunctionArgs) => {
+  console.log("Loader function called", context);
+  return null;
+};
+
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+      <h1>Welcome to Remix (with Vite and Cloudflare)</h1>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            href="https://developers.cloudflare.com/pages/framework-guides/deploy-a-remix-site/"
+            rel="noreferrer"
+          >
+            Cloudflare Pages Docs - Remix guide
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+            Remix Docs
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
